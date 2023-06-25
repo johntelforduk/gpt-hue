214355929b10aa57468433db35a15b0ae933c405
johntelforduk <jtelford1@gmail.com>
2023-06-25 23:38:52 +0100

    Refactor to use function calling.

--- README.md
+++ README.md
@@ -1,59 +1,49 @@
 # GPT Hue
 This program creates an [OpenAI](https://openai.com/) [GPT](https://en.wikipedia.org/wiki/Generative_pre-trained_transformer) powered chatbot which gives responses which control [Philips Hue](https://www.philips-hue.com/en-gb) lights using the [Hue API](https://github.com/studioimaginaire/phue). It is an experiment to see whether generative AI has the potential to make interacting with smart home devices easier.
 
-[Video demo on YouTube](https://www.youtube.com/watch?v=sAYGKsNVn6Y).
+It uses the recently released [function calling](https://openai.com/blog/function-calling-and-other-api-updates) capability in the Chat Completions API. For a great description of how this capability works, I recommend this [video](https://youtu.be/0lOSvOoF2to).
 
-### Installation
+### Example conversation
+**"turn on lights where we eat"**
+
+> Turned on Main Dining
+
+**"that's too bright"**
+
+> Main Dining set to 39% brightness
+
+**"still too bright"**
+
+> Main Dining set to 19% brightness
+
+**"turn them off"**
+
+> Turned off Main Dining
+
+### Setup
+The program requires an [OpenAI API key](https://platform.openai.com/account/api-keys).
+
+You also need to find out the IP address of the Hue Hub on your home network. You can find this out using the Hue app on your phone.
+
+Before running the program, create a `.env` file containing the following key/values,
 ```commandline
+# OpenAI
+OPEN_AI_KEY=<your OpenAI API key>
+OPEN_AI_MODEL="gpt-3.5-turbo-0613"
+
+# Philips Hue
+BRIDGE_IP=<IP address of your Hue Hub>
+```
+
+### Install & run
+```commandline
+pip install python-dotenv
 pip install phue
 pip install openai
 ```
-### First run
-Before first run of program,
-1. The program requires an `openai_key.txt` file containing your [OpenAI API key](https://platform.openai.com/account/api-keys).
-2. Edit `BRIDGE_IP` to be the actual IP address of the Hue Hub on your home network. You can find this out using the Hue app on your phone.
-3. Before the first run of the program, press the button on your Hue bridge (this is needed on the first run only).
-4. Finally, run the program with `python gpt_hue.py`
+Before the first run of the program, press the button on your Hue bridge (this is needed on the first run only).
 
-### The chatbot's mission
-At the start of the conversation, the GPT chatbot is asked to control some Hue lights using a defined API. The chatbot is also told a list of available lights (this list is obtained automatically using the Hue API). The chatbot is also given some additional information about the user's preferences - for example that it is a good idea to turn off lights in the Living Room before watching TV.
-
-Here is an example mission,
-
-> Hello.
-> I would like you to control the Hue light-bulbs in my house using API calls.
-> I will use natural language to ask you to do something with my Hue bulbs, and you should say the Hue API commands needed to do the thing I want.
->
-> Let's define an API for Hue lights as follow. To turn on a Hue light use `PUT/lights/<name>/state {"on": true}`
->
-> I have lights with the following names,
-> 
-> Floor Lamp\
-> Desk Lamp\
-> Main Dining\
-> Kitchen\
-> Living Room
->
-> I like to watch TV in the Living Room, with the lights in that room turned off. I do all of my cooking in the Kitchen.
->
-> Please ensure that all of your responses are 20 words or less. 
-> Please say "OK" now if you understand.
-
-
-### Example conversation
-**User input (or 'quit'):** 
->let's get ready for tv
-
-**ChatGPT:**
-> PUT/lights/Living Room/state {"on": false}\
-> Turned off Living Room
-
-**User input (or 'quit'):**
-> tv time is over
-
-**ChatGPT:**
-> PUT/lights/Living Room/state {"on": true}\
-> Turned on Living Room
+Finally, run the program with `python gpt_hue.py`
 
 ### Costs
 The program is currently configured to use the **gpt-3.5-turbo** model, which (at time of writing) has a [cost](https://openai.com/pricing) of $0.002 per 1,000 tokens. Note, new OpenAI accounts usually come with some token credits.
--- gpt_hue.py
+++ gpt_hue.py
@@ -1,8 +1,8 @@
 from phue import Bridge
 import openai
-
-MODEL = 'gpt-3.5-turbo'
-BRIDGE_IP = '192.168.1.193'
+from dotenv import load_dotenv
+from os import getenv
+import json
 
 
 def cost_calc(num_tokens: int) -> float:
@@ -15,36 +15,51 @@ def cost_calc(num_tokens: int) -> float:
 
 class Persona:
 
-    def __init__(self, name: str):
+    def __init__(self, gpt_model: str, persona_name: str, functions: list):
 
-        self.name = name
+        self.gpt_model = gpt_model
+        self.persona_name = persona_name
+        self.functions = functions
         self.history = []
         self.cumulative_tokens = 0
 
-    def give_mission(self, mission: str):
-        mission += '''\nPlease ensure that all of your responses are 20 words or less.
-Please say "OK" now if you understand.'''
-        print(self.name + ' mission...')
+    def give_mission(self, mission: str, response: str):
+        print(self.persona_name + ' mission...')
         print(mission)
         print('------------')
         self.update_history(role='user', content=mission)
 
         # 'Trick' GPT into thinking it understood us earlier in the conversation.
-        self.update_history(role='assistant', content='OK.')
+        self.update_history(role='assistant', content=response)
 
     def update_history(self, role: str, content: str):
         assert role in ['assistant', 'user']
         self.history.append({'role': role, 'content': content})
 
-    def chat(self, prompt: str) -> str:
+    def chat(self, prompt: str):
         self.update_history(role='user', content=prompt)
-        completion = openai.ChatCompletion.create(model=MODEL, messages=self.history)
-        self.cumulative_tokens += int(completion.usage.total_tokens)
-        response = completion.choices[0].message.content
 
-        print(self.name + ': ' + response)
-        self.update_history(role='assistant', content=response)
-        return response
+        completion = openai.ChatCompletion.create(model=self.gpt_model,
+                                                  messages=self.history,
+                                                  functions=self.functions,
+                                                  function_call="auto")
+
+        self.cumulative_tokens += int(completion.usage.total_tokens)
+        reply_content = completion.choices[0].message
+        # print(reply_content)
+
+        if 'function_call' in reply_content:
+            func_name = reply_content['function_call']['name']
+            args_json = reply_content.to_dict()['function_call']['arguments']
+            payload = json.loads(args_json)
+            payload['function_name'] = func_name
+            # print(payload)
+            return payload
+        else:
+            content = reply_content['content']
+            print(self.persona_name + ': ' + content)
+            self.update_history(role='assistant', content=content)
+            return {}
 
 
 class Lights:
@@ -82,62 +97,114 @@ class Lights:
         for each_light in remove_later:
             del self.lights[each_light]
 
-    def turn_on_or_off(self, name: str, on: bool):
-        if name not in self.lights:
+    def describe_lights(self) -> list:
+        """
+        Generate a list of the lights in the house, suitable for telling the chatbot about.
+        """
+        return [name for name in self.lights]
+
+    def turn_on_or_off(self, light_name: str, on: bool):
+        if light_name not in self.lights:
             print('Light not found.')
             return
 
-        # Turn on all the lights in the list.
-        for light_id in self.lights[name]:
+        # Adjust all of the lights in the list.
+        for light_id in self.lights[light_name]:
             self.bridge.set_light(int(light_id), 'on', on)
 
         if on:
-            print('Turned on ' + name)
+            print('Turned on ' + light_name)
         else:
-            print('Turned off ' + name)
+            print('Turned off ' + light_name)
 
-    def describe_lights(self) -> str:
+    def set_brightness(self, light_name: str, brightness: int):
+        if light_name not in self.lights:
+            print('Light not found.')
+            return
+
+        # Adjust all of the lights in the list.
+        for light_id in self.lights[light_name]:
+            self.bridge.set_light(int(light_id), {'on': True, 'bri': brightness})
+
+        print(light_name + ' set to ' + str(int(100 * brightness / 254)) + '% brightness')
+
+    def interpret_response(self, gpt_response: dict):
         """
-        Generate a description of the lights in the house, suitable for telling the chatbot about.
+        Interpret the response from the chatbot.
         """
-        description = '''I have lights with the following names,'''
-        for name in self.lights:
-            description += f'\n\t{name}'
-        return description
-
-    def interpret_response(self, response: str):
-        """Interpret the response from the chatbot. If it contains the word 'PUT', it means that the chatbot wants to
-        do a Hue API invocation. If that's the case, parse out the information in the PUT command, and
-        turn lights on or off accordingly.
-        """
-        if 'PUT' in response:
-            api_command = response.split('PUT')
-            api_parts = api_command[1].split('/')
-            light_name = api_parts[2]
-            state = 'true' in api_parts[3]
-
-            self.turn_on_or_off(light_name, state)
+        if len(gpt_response) == 0:
+            return
+        # print(gpt_response)
+        if gpt_response['function_name'] == 'turn_on_or_off':
+            self.turn_on_or_off(light_name=gpt_response['light_name'], on=gpt_response['on'])
+        elif gpt_response['function_name'] == 'set_brightness':
+            self.set_brightness(light_name=gpt_response['light_name'], brightness=gpt_response['brightness'])
 
 
-my_lights = Lights(BRIDGE_IP)
+load_dotenv(verbose=True)           # Set operating system environment variables based on contents of .env file.
+my_lights = Lights(getenv('BRIDGE_IP'))
+lights_list = my_lights.describe_lights()
+print('lights_list:', lights_list)
 
-openai.api_key = open('openai_key.txt', 'r').read().strip('\n')
-chatgpt = Persona('ChatGPT')
+# my_lights.turn_on_or_off(light_name='Master Bedroom', on=True)
+# my_lights.set_brightness(light_name='Master Bedroom', brightness=200)
 
-chatgpt.give_mission(mission='''Hello.
-I would like you to control the Hue light-bulbs in my house using API calls.
-I will use natural language to ask you to do something with my Hue bulbs, and you should say the Hue API commands 
-needed to do the thing I want.
-Let's define an API for Hue lights as follow. To turn on a Hue light use PUT/lights/<name>/state {"on": true}
-''' + my_lights.describe_lights() + '''
-I like to watch TV in the Living Room, with the lights in that room turned off. I do all of my cooking in the Kitchen.''')
+
+hue_functions = [
+{
+    "name": "turn_on_or_off",
+    "description": "Turn a Hue light bulb on or off.",
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "light_name": {
+                "type": "string",
+                "description": "The name of the Hue bulb that this function will turn on or off.",
+                "enum": lights_list
+            },
+            "on": {"type": "boolean",
+                   "description": "True if the light should be turned on. False if the light should be turned off."
+                   }
+        },
+        "required": ["light_name", "on"]
+    }
+},
+    {
+        "name": "set_brightness",
+        "description": "Change the level of brightness of a Hue bulb. Don't use this function for turning lights off.",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "light_name": {
+                    "type": "string",
+                    "description": "The name of the Hue bulb that this function will turn on or off.",
+                    "enum": lights_list
+                },
+                "brightness": {"type": "integer",
+                               "description": "The brightness that the bulb should be set to. Expressed as an integer between 0 and 254, where 0 is dark and 254 is maximum brightness.",
+                               "enum": list(range(255))
+                       }
+            },
+            "required": ["light_name", "brightness"]
+        }
+    }
+]
+
+openai.api_key = getenv('OPEN_AI_KEY')
+chatgpt = Persona(gpt_model=getenv('OPEN_AI_MODEL'), persona_name='ChatGPT', functions=hue_functions)
+
+mission = '''I'd like you to control the Philips Hue light bulbs in my house.
+Only use the set_brightness function for changing brightness. Make sure you use the turn_on_or_off function for actually turning the lights on and off.
+Please say "OK" now if you understand.'''
+response = 'OK.'
+chatgpt.give_mission(mission=mission, response=response)
 
 while True:
     inp = input("User input (or 'quit'): ")
     if inp == 'quit':
         break
     resp = chatgpt.chat(prompt=inp)
-    my_lights.interpret_response(resp)
+    my_lights.interpret_response(gpt_response=resp)
 
 print('\nTotal tokens used:', chatgpt.cumulative_tokens)
 print('Cost incurred (USD):', cost_calc(chatgpt.cumulative_tokens))
